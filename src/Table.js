3026118c92c2895bad13ea22f887dc777f3bdb20
4e65746f <ediwaldo.neto@outlook.com>
2022-08-28 10:41:37 -0300

    start project

---
+++ src/Table.js
@@ -0,0 +1,25 @@
+function Table(){
+    return (
+       <table>
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Nome</th>
+                <th>Marca</th>
+                <th>Selecionar</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+       </table>
+    )
+}
+
+export default Table;
