f3a1f55f2cdcea8f5b5cd7a34c59b62eb0127fbb
4e65746f <ediwaldo.neto@outlook.com>
2022-09-01 15:06:30 -0300

    adjust components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Form from './Form';
 import Table from './Table';
 
 function App() {
+
+  const [btnCadastrar, setBtnCadastrar] = useState(true);
+  const [produtos, setProdutos] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/product/")
+      .then(res => res.json())
+      .then(data => 
+        //console.log(data).
+        setProdutos(data.data))
+        
+  }, []);
+
   return (
-    <div>
-      <Form/>
-      <Table/>
-    </div>
+    <>
+    {produtos.length && 
+      <div>
+        <Form botao={btnCadastrar} />
+        <Table vetor={produtos} />
+      </div>
+    }
+    </>
+    
   );
 }
 
--- src/Table.js
+++ src/Table.js
@@ -1,24 +1,27 @@
-function Table(){
+function Table({ vetor }) {
     return (
-       <table className="table">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Nome</th>
-                <th>Marca</th>
-                <th>Selecionar</th>
-            </tr>
-        </thead>
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Nome</th>
+                    <th>Marca</th>
+                    
+                </tr>
+            </thead>
 
-        <tbody>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-        </tbody>
-       </table>
+            <tbody>
+                {
+                    vetor.map((element, index) => (
+                        <tr key={index}>
+                            <td>{index + 1}</td>
+                            <td>{element.nome}</td>
+                            <td>{element.marca}</td>
+                        </tr>
+                    ))
+                }
+            </tbody>
+        </table>
     )
 }
 
